51805a9d88bfe6f93e5d6970837bfb40552c1cb7
100co <100co@rice>
2024-08-23 23:41:29 +0900

    Bean

--- src/main/java/model/ReservationBean.java
+++ src/main/java/model/ReservationBean.java
@@ -1,9 +1,62 @@
 package model;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 public class ReservationBean implements Serializable {
 	
 	private int reservationId;
+	private String userId;
+	private Timestamp reservationDate;
+	private Timestamp updateDateTime;
+	
+	
+	
+	public int getReservationId() {
+		return reservationId;
+	}
+	
+	
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+	
+	
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+	
+	
+	
+	public Timestamp getReservationDate() {
+		return reservationDate;
+	}
+	
+	
+	public void setReservationDate(Timestamp reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+	
+	
+	
+	
+	
+	public Timestamp getUpdateDateTime() {
+		return updateDateTime;
+	}
+	public void setUpdateDateTime(Timestamp updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+	
+	
 	
 }
--- src/main/java/model/UserIdBean.java
+++ src/main/java/model/UserIdBean.java
@@ -6,5 +6,102 @@ public class UserIdBean implements Serializable{
 	
 	private int userId;
 	private String userName;
+	private String userEmail;
+	private String userPassword;
+	private String userAdress;
+	private String userPhone;
+	private int userPrivilege;
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+	
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+	
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	
+	
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+	
+	
+	
+	
+	public String getUserPassword() {
+		return userPassword;
+	}
+	
+	
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+	
+	
+	
+	public String getUserAdress() {
+		return userAdress;
+	}
+	
+	
+	public void setUserAdress(String userAdress) {
+		this.userAdress = userAdress;
+	}
+	
+	
+	
+	
+	
+	public String getUserPhone() {
+		return userPhone;
+	}
+	
+	
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	
+	
+	
+	
+	
+	public int getUserPrivilege() {
+		return userPrivilege;
+	}
+	
+	
+	public void setUserPrivilege(int userPrivilege) {
+		this.userPrivilege = userPrivilege;
+	}
+	
+	
+	
+	
 
 }
