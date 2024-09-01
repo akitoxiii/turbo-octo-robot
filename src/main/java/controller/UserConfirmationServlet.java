0efa47e4c8a7c99a90300102745b8f17694823f1
zeruq <zeruq@DESKTOP-EPHRPV3>
2024-09-02 08:54:42 +0900

    9/2

--- src/main/java/controller/UserConfirmationServlet.java
+++ src/main/java/controller/UserConfirmationServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/registerConfirmServlet")
+@WebServlet("/UserConfirmationServlet")
 public class UserConfirmationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
